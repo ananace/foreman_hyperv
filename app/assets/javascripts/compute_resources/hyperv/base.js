5febdb65a0f39d0630ac65467b45c22cabead785
Alexander Olofsson <alexander.olofsson@liu.se>
2017-08-22 15:57:29 +0200

    Setup for generation selection

--- app/assets/javascripts/compute_resources/hyperv/base.js
+++ app/assets/javascripts/compute_resources/hyperv/base.js
@@ -1,1 +1,6 @@
-alert('Specific Hyper-V')
+$(document).on('ContentLoad', hypervGenerationChange);
+
+function hypervGenerationChange() {
+    gen = $('#host_compute_attributes_generation').val()
+    // $('#host_compute_attributes_secure_boot_enabled').disable()
+}
