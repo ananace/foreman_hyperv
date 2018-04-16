d859bfa28903651e173a859351b38f50684123c7
Alexander Olofsson <alexander.olofsson@liu.se>
2018-04-16 11:29:51 +0200

    Add cluster host information to vm creation

--- app/assets/javascripts/foreman_hyperv/compute_resource_base.js
+++ app/assets/javascripts/foreman_hyperv/compute_resource_base.js
@@ -1,32 +1,42 @@
 function hypervGenerationChange(item) {
-    var toIter = ['#host_compute_attributes_secure_boot_enabled', '#compute_attribute_vm_attrs_secure_boot_enabled'];
-    gen = $(item).val();
+  var toIter = ['#host_compute_attributes_secure_boot_enabled', '#compute_attribute_vm_attrs_secure_boot_enabled'];
+  gen = $(item).val();
 
-    if (gen == 1) {
-        for (var i = 0; i < toIter.length; ++i) {
-            $(toIter[i]).attr('disabled', true);
-        }
-    } else {
-        for (var i = 0; i < toIter.length; ++i) {
-            $(toIter[i]).removeAttr('disabled');
-        }
+  if (gen == 1) {
+    for (var i = 0; i < toIter.length; ++i) {
+      $(toIter[i]).attr('disabled', true);
     }
+  } else {
+    for (var i = 0; i < toIter.length; ++i) {
+      $(toIter[i]).removeAttr('disabled');
+    }
+  }
 }
 
 function hypervDynamicMemoryChange(item) {
-    var toIter = [
-        '#host_compute_attributes_memory_maximum',
-        '#host_compute_attributes_memory_minimum',
-        '#compute_attribute_vm_attrs_memory_maximum',
-        '#compute_attribute_vm_attrs_memory_minimum',
-    ];
-    if (item.checked) {
-        for (var i = 0; i < toIter.length; ++i) {
-            $(toIter[i]).removeAttr('disabled');
-        }
-    } else {
-        for (var i = 0; i < toIter.length; ++i) {
-            $(toIter[i]).attr('disabled', true);
-        }
+  var toIter = [
+    '#host_compute_attributes_memory_maximum',
+    '#host_compute_attributes_memory_minimum',
+    '#compute_attribute_vm_attrs_memory_maximum',
+    '#compute_attribute_vm_attrs_memory_minimum',
+  ];
+
+  if (item.checked) {
+    for (var i = 0; i < toIter.length; ++i) {
+      $(toIter[i]).removeAttr('disabled');
     }
+  } else {
+    for (var i = 0; i < toIter.length; ++i) {
+      $(toIter[i]).attr('disabled', true);
+    }
+  }
+}
+
+function hypervHostChange(item) {
+  console.log('Hyper-V host changed to ' + $(item).val());
+
+  $('table.hyperv-host-info').hide();
+  $('table.hyperv-host-info[data-host="'+$(item).val()+'"]').show();
+
+  // TODO: Reload available switches
 }
