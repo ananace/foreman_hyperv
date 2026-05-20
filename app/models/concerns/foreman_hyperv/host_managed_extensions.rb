4f8e795d8e8a0c53b8a75d4e648417f6f68d79b0
Alexander Olofsson <alexander.olofsson@liu.se>
2026-05-20 11:16:39 +0200

    Slight improvement in NIC assignment

--- app/models/concerns/foreman_hyperv/host_managed_extensions.rb
+++ app/models/concerns/foreman_hyperv/host_managed_extensions.rb
@@ -5,47 +5,55 @@ module ForemanHyperv
     extend ActiveSupport::Concern
 
     def update(attributes = {})
-      if provider == 'Hyper-V' && attributes.key?('compute_attributes')
-        add_hyperv_attributes(attributes)
-        logger.debug "Expanded attributes to #{attributes}"
-      end
+      hyperv_add_attributes(attributes) if provider == 'Hyper-V' && attributes.key?('compute_attributes')
 
       super
     end
 
     def setComputeUpdate
-      return super unless provider == 'Hyper-V'
+      ret = super
+      return ret unless provider == 'Hyper-V'
 
-      attr = compute_attributes.dup
+      begin
+        hyperv_sync_interfaces
+      rescue => e
+        failure _("Failed to update a compute %{compute_resource} instance %{name}: %{e}") %
+                { compute_resource:, name:, e: }, e
+      end
+      true
+    end
 
-      super
-
+    def hyperv_sync_interfaces
       unmapped_ifaces = interfaces.select { |iface| iface.physical? && !iface.compute_attributes['identity'].present? }
-      return if unmapped_ifaces.empty?
+      return true if unmapped_ifaces.empty?
 
-      logger.info "Mapping unknown interfaces after update for #{name}"
+      logger.info "Mapping #{unmapped_ifaces.count} unknown interfaces for #{name}"
 
       self.vm = compute_object
       fog_nics = vm.interfaces.dup
       interfaces.each { |iface| fog_nics.delete_if { |vmiface| vmiface.id == iface.compute_attributes['identity'] } }
 
       unmapped_ifaces.each do |nic|
-        logger.info "Matching #{nic.inspect} against #{fog_nics}"
+        logger.debug "Matching #{nic.inspect} against #{fog_nics}"
         selected_nic = vm.select_nic(fog_nics, nic)
         if selected_nic.nil?
           logger.warn "Orchestration::Compute: Could not match network interface #{nic.inspect}"
-          return failure(_("Could not find virtual machine network interface matching %s") % [nic.identifier, nic.ip, nic.name, nic.type].find(&:present?))
+          raise ArgumentError, \
+            _("Could not find virtual machine network interface matching %s") %
+            [nic.identifier, nic.ip, nic.name, nic.type].find(&:present?)
         end
 
         logger.debug "Orchestration::Compute: nic #{nic.inspect} assigned to #{selected_nic.inspect}"
         nic.mac ||= selected_nic.mac
         nic.save
+        fog_nics.delete selected_nic
       end
     end
 
     private
 
-    def add_hyperv_attributes(attributes)
+    # Inject interface attributes into the compute values, to allow modifying Hyper-V configuration on hosts
+    def hyperv_add_attributes(attributes)
       compute = attributes['compute_attributes'] ||= {}
       compute['interfaces_attributes'] ||= {}
 
