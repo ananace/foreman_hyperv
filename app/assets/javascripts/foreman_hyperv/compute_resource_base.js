ade9d94d35b34bed4eba97689b1516f9ba42b064
Alexander Olofsson <alexander.olofsson@liu.se>
2026-06-11 13:01:42 +0200

    Clean up and slightly beautify HTML/JS

--- app/assets/javascripts/foreman_hyperv/compute_resource_base.js
+++ app/assets/javascripts/foreman_hyperv/compute_resource_base.js
@@ -1,20 +1,3 @@
-$(document).on('ContentLoad', function() { tfm.numFields.initAll(); });
-
-function hypervGenerationChange(item) {
-  var toIter = ['#host_compute_attributes_secure_boot_enabled', '#compute_attribute_vm_attrs_secure_boot_enabled'];
-  gen = $(item).val();
-
-  if (gen == 'BIOS') {
-    for (var i = 0; i < toIter.length; ++i) {
-      $(toIter[i]).attr('disabled', true);
-    }
-  } else {
-    for (var i = 0; i < toIter.length; ++i) {
-      $(toIter[i]).removeAttr('disabled');
-    }
-  }
-}
-
 function hypervDynamicMemoryChange(item) {
   var toIter = [
     '#host_compute_attributes_memory_maximum',
